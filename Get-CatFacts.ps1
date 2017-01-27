eeec85252c04ec7ce6695ce6d090b4b40d71e970
Chuck Danger <beneth@gmail.com>
2017-01-27 11:17:01 -0600

    Added standalone function and module files.
    Added standalone function and module files.

---
+++ Get-CatFacts.ps1
@@ -0,0 +1,41 @@
+﻿<#
+.Synopsis
+   Get one (or more!) wonderful facts about cats!
+.DESCRIPTION
+   What more do you want? Just read some cat facts, nerd...
+.EXAMPLE - Just get one cat fact
+   Get-CatFacts
+.EXAMPLE - Get 10 cat facts!
+   Get-CatFacts -Number 10
+#>
+function Get-CatFacts
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([string])]
+    Param
+    (
+        # Number of cat facts to obtain
+        [Parameter(Mandatory=$false,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Number
+    )
+
+    if (!$number)
+    {
+        $facts = Invoke-RestMethod -Uri http://catfacts-api.appspot.com/api/facts
+    }
+    else
+    {
+        $facts = Invoke-RestMethod -Uri http://catfacts-api.appspot.com/api/facts?number=$number
+    }
+    
+    foreach ($fact in $facts.facts)
+    {
+        Write-Output "CATFACT: $fact"
+    }
+
+}
+
